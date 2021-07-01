c70f1ec1e81ef9bc45b85649e267c572e5ef7ee1
Abdulaziz Almohammadi <AbdulazizAlmohammadi@users.noreply.github.com>
2021-07-01 17:45:01 +0300

    add XMLWriter class with all functions

---
+++ XmlProject/XmlProject/XMLWriter.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml;
+
+namespace XmlProject
+{
+    public class XMLWriter
+    {
+        private XmlDocument xmldoc;
+        private XmlElement root;
+
+        public XMLWriter(){
+            this.xmldoc = new XmlDocument();
+            this.root = xmldoc.CreateElement("Members");
+        }
+
+        public XMLWriter CreateMember(string name , string major){
+            XmlElement element = xmldoc.CreateElement("Member");
+            XmlElement nameElement = xmldoc.CreateElement("Name");
+            XmlElement majorElement = xmldoc.CreateElement("Major");
+
+            XmlText textNode = xmldoc.CreateTextNode(name);
+            XmlText textNode2 = xmldoc.CreateTextNode(major);
+
+            nameElement.AppendChild(textNode);
+            majorElement.AppendChild(textNode2);  
+
+            element.AppendChild(nameElement);
+            element.AppendChild(majorElement);
+            root.AppendChild(element);
+
+            return this;
+        }
+
+        public XMLWriter save(string path){
+        
+            xmldoc.Save(path);
+
+            return this;
+        }
+    }
+}
