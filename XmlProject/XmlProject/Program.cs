7b689e4068a9f006c2c5c5a979db31851bb14946
Yasmin <yasmin.alghamd@tuwaiq.edu.sa>
2021-07-01 16:16:15 +0300

    Added XmlProject

---
+++ XmlProject/XmlProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XmlProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
