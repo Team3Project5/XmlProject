fe6d8f8eda3c5042bb74ebb562b0624f33c7b588
Abdulaziz Almohammadi <AbdulazizAlmohammadi@users.noreply.github.com>
2021-07-01 17:50:08 +0300

    Fix createMember

--- XmlProject/XmlProject/XMLWriter.cs
+++ XmlProject/XmlProject/XMLWriter.cs
@@ -15,22 +15,18 @@ namespace XmlProject
         public XMLWriter CreateMember(string name , string major){
             XmlElement element = xmldoc.CreateElement("Member");
             XmlElement nameElement = xmldoc.CreateElement("Name");
-            XmlElement majorElement = xmldoc.CreateElement("Major");
 
             XmlText textNode = xmldoc.CreateTextNode(name);
-            XmlText textNode2 = xmldoc.CreateTextNode(major);
 
             nameElement.AppendChild(textNode);
-            majorElement.AppendChild(textNode2);  
 
             element.AppendChild(nameElement);
-            element.AppendChild(majorElement);
             root.AppendChild(element);
 
             return this;
         }
 
-        public XMLWriter save(string path){
+        public XMLWriter Save(string path){
         
             xmldoc.Save(path);
 
